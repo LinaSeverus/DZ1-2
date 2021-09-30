fad63edc992a4353cb2b2f285aac5e0151cc5ca2
LinaSeverus <severus@ukr.net>
2021-09-30 20:44:23 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let num1;
+let num2;
+let result;
+
+num1 = prompt('Напишите первое число');
+num2 = prompt('Напишите второе число');
+result = +num1 + +num2;
+alert(`Результат: ${num1} + ${num2} = ${result}`);
+
+num1 = prompt('Напишите первое число');
+num2 = prompt('Напишите второе число');
+result = num1 - num2;
+alert(`Результат: ${num1} - ${num2} = ${result}`);
+
+num1 = prompt('Напишите первое число');
+num2 = prompt('Напишите второе число');
+result = num1 * num2;
+alert(`Результат: ${num1} * ${num2} = ${result}`);
+
+num1 = prompt('Напишите первое число');
+num2 = prompt('Напишите второе число');
+result = num1 / num2;
+alert(`Результат: ${num1} / ${num2} = ${result}`);
